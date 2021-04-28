e2b403e2b89b7fdf0ed6469e67938514459df472
Hwang Jae Hwan <zxcv0069@naver.com>
2021-04-29 01:29:31 +0900

    study

--- src/main/java/com/example/hellospring/HelloSpringApplication.java
+++ src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -2,9 +2,11 @@ package com.example.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloSpringApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,51 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //@Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+        //같은 이름 회원 존재 x
+        duplicateMember(member);//중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void duplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+        .ifPresent(member1 -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     *전체 조회
+     */
+
+    public List<Member> findMembers() {
+
+        return memberRepository.findAll();
+
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
