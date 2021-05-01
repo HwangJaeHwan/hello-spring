9ead9664124dcd15d51411a4ad54ecb6af98e66b
Hwang Jae Hwan <zxcv0069@naver.com>
2021-05-01 23:23:24 +0900

    study

--- src/main/java/com/example/hellospring/sop/TimeTraceApp.java
+++ src/main/java/com/example/hellospring/sop/TimeTraceApp.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class TimeTraceApp {
-    @Around("execution(* com.example.hellospring.service.*(..))")
+    @Around("execution(* com.example.hellospring..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("Start : "+ joinPoint.toString() + " " + start+"ms");
